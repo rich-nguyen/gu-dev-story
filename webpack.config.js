323ce971113cd364d31557ed5a2bf533f2243f61
Richard Nguyen <richard.nguyen@guardian.co.uk>
2017-11-27 22:05:42 +0000

    Add phaser

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+var path = require('path');
+
+var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
+var phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+var pixi = path.join(phaserModule, 'build/custom/pixi.js');
+var p2 = path.join(phaserModule, 'build/custom/p2.js');
+
 module.exports = {
     entry: "./src/app.ts",
     output: {
         filename: "./dist/bundle.js"
     },
     resolve: {
-        extensions: [".ts"]
+        extensions: [".ts"],
+        alias: {
+          'phaser': phaser,
+          'pixi': pixi,
+          'p2': p2,
+        }
     },
     module: {
         rules: [
@@ -14,6 +26,18 @@ module.exports = {
                 use: {
                     loader: "ts-loader"
                 }
+            },
+            {
+                test: /pixi\.js/,
+                loader: 'expose-loader?PIXI'
+            },
+            {
+                test: /phaser-split\.js$/,
+                loader: 'expose-loader?Phaser'
+            },
+            {
+                test: /p2\.js/,
+                loader: 'expose-loader?p2'
             }
         ]
     }
